618fb467483057e1ee21680dfb87afe97e75937d
chessai <chessai1996@gmail.com>
2020-11-19 16:40:16 -0800

    Document: use PrimArray internally instead of Array

--- Duckling/Types/Document.hs
+++ Duckling/Types/Document.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Duckling.Types.Document
   ( Document -- abstract
@@ -18,43 +19,43 @@ module Duckling.Types.Document
   , isRangeValid
   ) where
 
-import Data.Array.Unboxed (UArray)
+import Control.Monad.ST (ST, runST)
 import Data.ByteString (ByteString)
 import Data.List (scanl', foldl', foldr)
+import Data.Primitive.PrimArray
 import Data.String
 import Data.Text (Text)
 import Prelude hiding (length)
-import qualified Data.Array.Unboxed as Array
 import qualified Data.ByteString as BS
 import qualified Data.Char as Char
-import qualified Data.Text.Unsafe as UText
-import qualified Data.Text.Encoding as Text
+import qualified Data.Primitive.PrimArray as PArray
 import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
 import qualified Data.Text.Internal.Unsafe.Char as UText
-
+import qualified Data.Text.Unsafe as UText
 
 data Document = Document
-  { rawInput :: !Text
-  , utf8Encoded :: ByteString
-  , indexable :: UArray Int Char -- for O(1) indexing pos -> Char
-  , firstNonAdjacent :: UArray Int Int
+  { rawInput :: {-# unpack #-} !Text
+  , utf8Encoded :: {-# unpack #-} !ByteString
+  , indexable :: {-# unpack #-} !(PrimArray Char) -- for O(1) indexing pos -> Char
+  , firstNonAdjacent :: {-# unpack #-} !(PrimArray Int)
     -- for a given index 'i' it keeps a first index 'j' greater or equal 'i'
     -- such that isAdjacentSeparator (indexable ! j) == False
     -- eg. " a document " :: Document
     --     firstNonAdjacent = [1,1,3,3,4,5,6,7,8,9,10,12]
     -- Note that in this case 12 is the length of the vector, hence not a
     -- valid index inside the array, this is intentional.
-  , tDropToBSDrop :: UArray Int Int
+  , tDropToBSDrop :: {-# unpack #-} !(PrimArray Int)
     -- how many bytes to BS.drop from a utf8 encoded ByteString to
     -- reach the same position as Text.drop would
-  , bsDropToTDrop :: UArray Int Int
+  , bsDropToTDrop :: {-# unpack #-} !(PrimArray Int)
     -- the inverse of tDropToBSDrop, rounds down for bytes that are
     -- not on character boundary
     -- for "żółty" :: Document
     --   tDropToBSDrop = [0,2,4,6,7,8]
     --   bsDropToTDrop = [0,1,1,2,2,3,3,4,5]
     --   tDropToUtf16Drop = [0,1,2,3,4,5]
-  , tDropToUtf16Drop :: UArray Int Int
+  , tDropToUtf16Drop :: {-# unpack #-} !(PrimArray Int)
     -- translate Text.drop to Data.Text.Unsafe.dropWord16
   } deriving (Show)
 
@@ -63,7 +64,7 @@ Note [Regular expressions and Text]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Text is UTF-16 encoded internally and PCRE operates on UTF-8 encoded
 ByteStrings. Because we do a lot of regexp matching on the same Text,
-it pays off to cache UTF-8 the encoded ByteString. That's the utf8Ecoded
+it pays off to cache UTF-8 the encoded ByteString. That's the utf8Encoded
 field in Document.
 
 Moreover we do regexp matching with capture, where the captured groups
@@ -85,19 +86,24 @@ fromText rawInput = Document{..}
   utf8Encoded = Text.encodeUtf8 rawInput
   rawInputLength = Text.length rawInput
   unpacked = Text.unpack rawInput
-  indexable = Array.listArray (0, rawInputLength - 1) unpacked
-  firstNonAdjacent = Array.listArray (0, rawInputLength - 1) $ snd $
-   foldr gen (rawInputLength, []) $ zip [0..] unpacked
+  indexable = primArrayFromListN rawInputLength unpacked
+  firstNonAdjacent = primArrayFromListN rawInputLength
+    $ snd
+    $ foldr gen (rawInputLength, [])
+    $ zip [0..] unpacked
   -- go from the end keeping track of the first nonAdjacent (best)
   gen (ix, elem) (best, !acc)
     | isAdjacentSeparator elem = (best, best:acc)
     | otherwise = (ix, ix:acc)
   tDropToBSDropList = scanl' (\acc a -> acc + utf8CharWidth a) 0 unpacked
-  tDropToBSDrop = Array.listArray (0, rawInputLength) tDropToBSDropList
-  tDropToUtf16Drop = Array.listArray (0, rawInputLength) $
-    scanl' (\acc a -> acc + utf16CharWidth a) 0 unpacked
-  bsDropToTDrop = Array.listArray (0, BS.length utf8Encoded) $
-    reverse $ snd $ foldl' fun (-1, []) $ zip [0..] tDropToBSDropList
+  tDropToBSDrop = primArrayFromListN (rawInputLength + 1) tDropToBSDropList
+  tDropToUtf16Drop = primArrayFromListN (rawInputLength + 1)
+    $ scanl' (\acc a -> acc + utf16CharWidth a) 0 unpacked
+  bsDropToTDrop = primArrayFromListN (BS.length utf8Encoded + 1)
+    $ reverse
+    $ snd
+    $ foldl' fun (-1, [])
+    $ zip [0..] tDropToBSDropList
   fun (lastPos, !acc) (ix, elem) = (elem, replicate (elem - lastPos) ix ++ acc)
   utf8CharWidth c
     | w <= 0x7F = 1
@@ -131,17 +137,17 @@ isRangeValid doc start end =
 
 -- True iff a is followed by whitespaces and b.
 isAdjacent :: Document -> Int -> Int -> Bool
-isAdjacent Document{..} a b =
-  b >= a && (firstNonAdjacent Array.! a >= b)
+isAdjacent (Document{..}) a b =
+  b >= a && (indexPrimArray firstNonAdjacent a >= b)
 
 isAdjacentSeparator :: Char -> Bool
 isAdjacentSeparator c = elem c [' ', '\t']
 
 (!) :: Document -> Int -> Char
-(!) Document { indexable = s } ix = s Array.! ix
+(!) (Document {..}) ix = indexPrimArray indexable ix
 
 length :: Document -> Int
-length Document { indexable = s } = Array.rangeSize $ Array.bounds s
+length (Document {..}) = sizeofPrimArray indexable
 
 -- | Given a document and an offset (think Text.drop offset),
 -- returns a utf8 encoded substring of Document at that offset
@@ -161,18 +167,12 @@ byteStringFromPos
      , (Int, Int) -> Text
      , Int -> Int -> (Int, Int)
      )
-byteStringFromPos
-  Document { rawInput = rawInput
-           , utf8Encoded = utf8Encoded
-           , tDropToBSDrop = tDropToBSDrop
-           , bsDropToTDrop = bsDropToTDrop
-           , tDropToUtf16Drop = tDropToUtf16Drop
-           }
-  position = (substring, rangeToText, translateRange)
+byteStringFromPos Document {..} position
+  = (substring, rangeToText, translateRange)
   where
   -- See Note [Regular expressions and Text] to understand what's going
   -- on here
-  utf8Position = tDropToBSDrop Array.! position
+  utf8Position = indexPrimArray tDropToBSDrop position
   substring :: ByteString
   substring = BS.drop utf8Position utf8Encoded
   -- get a subrange of Text reusing the underlying buffer using
@@ -183,8 +183,8 @@ byteStringFromPos
   rangeToText r = UText.takeWord16 (end16Pos - start16Pos) $
     UText.dropWord16 start16Pos rawInput
     where
-    start16Pos = tDropToUtf16Drop Array.! startPos
-    end16Pos = tDropToUtf16Drop Array.! endPos
+    start16Pos = indexPrimArray tDropToUtf16Drop startPos
+    end16Pos = indexPrimArray tDropToUtf16Drop endPos
     (startPos, endPos) = uncurry translateRange r
   -- from utf8 offset and length to Text character start and end position
   translateRange :: Int -> Int -> (Int, Int)
@@ -193,5 +193,21 @@ byteStringFromPos
     res = (startPos, endPos)
     realBsStart = utf8Position + bsStart
     realBsEnd = realBsStart + bsLen
-    startPos = bsDropToTDrop Array.! realBsStart
-    endPos = bsDropToTDrop Array.! realBsEnd
+    startPos = indexPrimArray bsDropToTDrop realBsStart
+    endPos = indexPrimArray bsDropToTDrop realBsEnd
+
+primArrayFromText :: Text -> PrimArray Char
+primArrayFromText txt = runST go
+  where
+    go :: forall s. ST s (PrimArray Char)
+    go = do
+      buf <- newPrimArray (Text.length txt)
+      Text.foldl' -- smh no foldM
+        (\ixM c -> do
+            ix <- ixM
+            writePrimArray buf ix c
+            pure (ix + 1)
+        )
+        (pure 0)
+        txt
+      unsafeFreezePrimArray buf
